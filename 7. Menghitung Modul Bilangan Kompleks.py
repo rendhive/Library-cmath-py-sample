7c83345c9d80850c518775e55b56f3c4e565de0c
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:34:32 +0700

    [FEATURE][DOCS] Modulus kompleks
    Menambahkan contoh perhitungan modulus bilangan kompleks.
    Mencakup pembuatan z, penggunaan abs(z), serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 7. Menghitung Modul Bilangan Kompleks.py
@@ -0,0 +1,7 @@
+import cmath
+
+z = complex(3, 4)
+modulus = abs(z)
+print("Modulus dari bilangan kompleks:", modulus)
+# Fungsi: Menghitung modulus (atau panjang) dari bilangan kompleks.
+# Kondisi: Ketika Anda perlu mengetahui jarak bilangan kompleks dari titik asal.
