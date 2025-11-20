87b052998102c71112334c10d95519cf75623eb6
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:20:14 +0700

    Add files via upload
    [FEATURE][DOCS] Menambahkan import cmath dan dokumentasi penggunaan modul untuk operasi bilangan kompleks

---
+++ 1. Mengimpor Modul cmath.py
@@ -0,0 +1,5 @@
+import cmath
+
+print("Modul cmath berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk melakukan operasi pada bilangan kompleks.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari modul cmath.
