69c33e602fade678ccef5c63dc9d01ae4e0d463e
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:27:33 +0700

    [FEATURE][DOCS] Penjumlahan kompleks
    Menambahkan contoh operasi penjumlahan dua bilangan kompleks.
    Termasuk pembuatan z1 dan z2, operasi z1 + z2, serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 3. Menjumlahkan Bilangan Kompleks.py
@@ -0,0 +1,8 @@
+import cmath
+
+z1 = complex(1, 2)
+z2 = complex(2, 3)
+result = z1 + z2
+print("Hasil penjumlahan:", result)
+# Fungsi: Menjumlahkan dua bilangan kompleks.
+# Kondisi: Ketika Anda ingin melakukan operasi penjumlahan pada bilangan kompleks.
