aa9b407dac3a4447cbc14a58fad135b8b293b269
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:32:25 +0700

    [FEATURE][DOCS] Pembagian kompleks
    Menambahkan contoh operasi pembagian bilangan kompleks.
    Mencakup pembuatan z1 dan z2, operasi z1 / z2, serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 6. Membagi Bilangan Kompleks.py
@@ -0,0 +1,8 @@
+import cmath
+
+z1 = complex(3, 2)
+z2 = complex(1, 1)
+result = z1 / z2
+print("Hasil pembagian:", result)
+# Fungsi: Membagi satu bilangan kompleks dengan yang lain.
+# Kondisi: Saat Anda perlu menghitung rasio antara dua bilangan kompleks.
