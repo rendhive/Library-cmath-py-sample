a0083b8642ac93717a2de3dd391f07c683fd9ecf
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:30:59 +0700

    [FEATURE][DOCS] Perkalian kompleks
    Menambahkan contoh operasi perkalian dua bilangan kompleks.
    Mencakup pembuatan z1 dan z2, operasi z1 * z2, serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 5. Mengalikan Bilangan Kompleks.py
@@ -0,0 +1,8 @@
+import cmath
+
+z1 = complex(1, 2)
+z2 = complex(3, 4)
+result = z1 * z2
+print("Hasil perkalian:", result)
+# Fungsi: Mengalikan dua bilangan kompleks.
+# Kondisi: Ketika Anda ingin menghitung hasil kali dari bilangan kompleks.
