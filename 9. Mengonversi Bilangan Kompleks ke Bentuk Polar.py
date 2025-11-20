d912127b3273c2036c5d27be16be6e9f186096e0
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:37:22 +0700

    [FEATURE][DOCS] Bentuk polar kompleks
    Menambahkan contoh konversi bilangan kompleks ke bentuk polar menggunakan cmath.polar().
    Termasuk pembuatan z serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 9. Mengonversi Bilangan Kompleks ke Bentuk Polar.py
@@ -0,0 +1,7 @@
+import cmath
+
+z = complex(1, 1)
+polar = cmath.polar(z)
+print("Bentuk polar (modulus, sudut):", polar)
+# Fungsi: Mengonversi bilangan kompleks ke bentuk polar.
+# Kondisi: Ketika Anda ingin menggunakan representasi polar dari bilangan kompleks.
