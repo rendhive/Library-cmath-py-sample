ba4170897e50b76dbf7a7d7c5029b30d0e4f36a5
Rendhive code <rendhivecode@gmail.com>
2025-11-21 05:29:07 +0700

    [FEATURE][DOCS] Pengurangan kompleks
    Menambahkan contoh operasi pengurangan dua bilangan kompleks.
    Mencakup pembuatan z1 dan z2, operasi z1 - z2, serta dokumentasi fungsi dan kondisi penggunaannya.

---
+++ 4. Mengurangkan Bilangan Kompleks.py
@@ -0,0 +1,8 @@
+import cmath
+
+z1 = complex(5, 4)
+z2 = complex(2, 3)
+result = z1 - z2
+print("Hasil pengurangan:", result)
+# Fungsi: Mengurangkan satu bilangan kompleks dari yang lain.
+# Kondisi: Ketika Anda perlu menghitung selisih antara dua bilangan kompleks.
