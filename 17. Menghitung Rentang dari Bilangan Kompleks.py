564e07cfdb5a12dfd714966faf16d8e841105734
Rendhive code <rendhivecode@gmail.com>
2025-11-21 19:51:54 +0700

    [FEATURE][DOCS] Jarak dua kompleks
    Menghitung jarak (range) antara dua bilangan kompleks menggunakan abs(z1 - z2).
    Termasuk contoh kode, fungsi, dan kondisi penggunaan.

---
+++ 17. Menghitung Rentang dari Bilangan Kompleks.py
@@ -0,0 +1,8 @@
+import cmath
+
+z1 = complex(2, 3)
+z2 = complex(3, 4)
+range_value = abs(z1 - z2)
+print("Rentang antara dua bilangan kompleks (jarak):", range_value)
+# Fungsi: Menghitung jarak antara dua bilangan kompleks.
+# Kondisi: Ketika Anda memperhitungkan jarak langsung antara titik kompleks.
