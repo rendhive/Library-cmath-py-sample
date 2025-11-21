85907f991dea8d0916f91ce8060cb24f120f0a1b
Rendhive code <rendhivecode@gmail.com>
2025-11-21 15:54:40 +0700

    [FEATURE][DOCS] Polar ke kompleks
    Menambahkan contoh konversi dari bentuk polar ke bilangan kompleks menggunakan cmath.rect().
    Mencakup tuple polar dan dokumentasi fungsi serta kondisi penggunaan.

---
+++ 10. Mengonversi dari Polar ke Kompleks.py
@@ -0,0 +1,7 @@
+import cmath
+
+polar = (1, cmath.pi / 4)
+z = cmath.rect(*polar)
+print("Bilangan kompleks dari bentuk polar:", z)
+# Fungsi: Mengonversi dari bentuk polar kembali ke bilangan kompleks.
+# Kondisi: Ketika Anda perlu mengubah representasi dari polar ke kartesian.
