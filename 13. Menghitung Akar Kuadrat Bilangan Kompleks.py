77d20c059b24c74942d3efec8a9e2a3339e4834d
Rendhive code <rendhivecode@gmail.com>
2025-11-21 15:58:54 +0700

    [FEATURE][DOCS] Akar kompleks
    Menambahkan contoh penggunaan cmath.sqrt() untuk menghitung akar kuadrat bilangan kompleks.
    Termasuk pembuatan z serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 13. Menghitung Akar Kuadrat Bilangan Kompleks.py
@@ -0,0 +1,7 @@
+import cmath
+
+z = complex(3, 4)
+result = cmath.sqrt(z)
+print("Akar kuadrat dari bilangan kompleks:", result)
+# Fungsi: Menghitung akar kuadrat dari bilangan kompleks.
+# Kondisi: Ketika Anda perlu menemukan akar kuadrat dalam konteks kompleks.
