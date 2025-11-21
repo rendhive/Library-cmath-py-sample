cd1dcfae7cf10b2eafbd309447e25840d8db9cd4
Rendhive code <rendhivecode@gmail.com>
2025-11-21 19:53:24 +0700

    [FEATURE][DOCS] Penetapan variabel kompleks
    Menambahkan contoh penetapan bilangan kompleks ke variabel untuk penggunaan ulang.
    Termasuk fungsi dan kondisi penggunaan.

---
+++ 18. Menetapkan Bilangan Kompleks ke Variabel.py
@@ -0,0 +1,7 @@
+import cmath
+
+z = 4 + 3j
+u = z
+print("Bilangan kompleks yang ditetapkan:", u)
+# Fungsi: Mengaitkan bilangan kompleks ke dalam variabel.
+# Kondisi: Saat Anda ingin menggunakan satu bilangan kompleks di berbagai bagian kode.
