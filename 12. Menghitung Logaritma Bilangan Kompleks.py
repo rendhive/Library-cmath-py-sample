75343fab8ac824cc56f0184a2ec198102e01def3
Rendhive code <rendhivecode@gmail.com>
2025-11-21 15:57:06 +0700

    [FEATURE][DOCS] Logaritma kompleks
    Menambahkan contoh penggunaan cmath.log() untuk menghitung logaritma bilangan kompleks.
    Termasuk pembuatan z serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 12. Menghitung Logaritma Bilangan Kompleks.py
@@ -0,0 +1,7 @@
+import cmath
+
+z = complex(1, 1)
+result = cmath.log(z)
+print("Logaritma dari bilangan kompleks:", result)
+# Fungsi: Menghitung logaritma dari bilangan kompleks.
+# Kondisi: Ketika Anda perlu menghitung logaritma dengan basis kompleks.
