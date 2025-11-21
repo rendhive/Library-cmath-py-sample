cf337bd777200d4c4ed382a6de72faf792e3fe6f
Rendhive code <rendhivecode@gmail.com>
2025-11-21 19:41:49 +0700

    [FEATURE][DOCS] Kuadrat bilangan kompleks
    Menambahkan contoh operasi kuadrat bilangan kompleks imaginer murni menggunakan z ** 2.
    Termasuk pembuatan z serta dokumentasi fungsi dan kondisi penggunaan.

---
+++ 15. Menghitung Bilangan Kompleks Kuadrat Imaginer.py
@@ -0,0 +1,7 @@
+import cmath
+
+z = complex(0, 3)
+result = z ** 2
+print("Kuadrat dari bilangan kompleks imaginer (j^2):", result)
+# Fungsi: Menghitung kuadrat dari bilangan kompleks imaginer.
+# Kondisi: Ketika Anda ingin menghitung pangkat bilangan kompleks yang imaginernya murni.
